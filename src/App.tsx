14016133e7005f74bb29463ad34189dce43bc4f7
LukeMcCann <developer-lukemccann@outlook.com>
2022-04-22 21:01:45 +0100

    Add app context

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import './App.css';
 
 import { Person, HairColor } from './components/Person';
 
+interface IAppContext {
+  name: string,
+  age: number,
+  country: string,
+};
+
+const AppContext = createContext<IAppContext | null>(null);
+
+const contextValue: IAppContext = {
+  name: 'Luke',
+  age: 27,
+  country: 'England',
+};
+
 export const App : React.FC = () => {
   return (
-    <div className='App'>
-      <Person
-        name="Luke"
-        email="developer-lukemccann@outlook.com"
-        gender='Male'
-        age={27}
-        hairColor={HairColor.Black}
-      />
-    </div>
+    <AppContext.Provider value={contextValue}>
+      <div className='App'>
+        <Person
+          name="Luke"
+          email="developer-lukemccann@outlook.com"
+          gender='Male'
+          age={27}
+          hairColor={HairColor.Black}
+        />
+      </div>
+    </AppContext.Provider>
   );
 }
